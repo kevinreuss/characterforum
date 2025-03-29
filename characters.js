8270314c51ab37acf307bfccfd0bb5a29fb389f3
Kevin Reuss <kevreuss@gmail.com>
2025-03-29 01:20:21 +0100

    improve

--- characters.js
+++ characters.js
@@ -278,4 +278,522 @@ const characters = [
       "https://static.wikia.nocookie.net/sherlock/images/b/bc/John_Watson.jpg",
     appearances: ["Sherlock"],
   },
+  {
+    id: 36,
+    name: "Daryl Dixon",
+    series: "The Walking Dead",
+    image:
+      "https://static.wikia.nocookie.net/walkingdead/images/e/e8/Daryl_Dixon.jpg",
+    appearances: ["The Walking Dead"],
+  },
+  {
+    id: 37,
+    name: "Dustin Henderson",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/7/7b/Dustin_Henderson.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 38,
+    name: "Mike Wheeler",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/f/f0/Mike_Wheeler.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 39,
+    name: "Gus Fring",
+    series: "Breaking Bad",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/6/6f/Gus_Fring.jpg",
+    appearances: ["Breaking Bad", "Better Call Saul"],
+  },
+  {
+    id: 40,
+    name: "Marshall Eriksen",
+    series: "How I Met Your Mother",
+    image:
+      "https://static.wikia.nocookie.net/himym/images/8/8a/Marshall_Eriksen.jpg",
+    appearances: ["How I Met Your Mother"],
+  },
+  {
+    id: 41,
+    name: "Lily Aldrin",
+    series: "How I Met Your Mother",
+    image:
+      "https://static.wikia.nocookie.net/himym/images/e/e2/Lily_Aldrin.jpg",
+    appearances: ["How I Met Your Mother"],
+  },
+  {
+    id: 42,
+    name: "Robin Scherbatsky",
+    series: "How I Met Your Mother",
+    image:
+      "https://static.wikia.nocookie.net/himym/images/4/4e/Robin_Scherbatsky.jpg",
+    appearances: ["How I Met Your Mother"],
+  },
+  {
+    id: 43,
+    name: "Raj Koothrappali",
+    series: "The Big Bang Theory",
+    image:
+      "https://static.wikia.nocookie.net/bigbangtheory/images/4/4c/Raj_Koothrappali.jpg",
+    appearances: ["The Big Bang Theory"],
+  },
+  {
+    id: 44,
+    name: "Howard Wolowitz",
+    series: "The Big Bang Theory",
+    image:
+      "https://static.wikia.nocookie.net/bigbangtheory/images/5/5c/Howard_Wolowitz.jpg",
+    appearances: ["The Big Bang Theory"],
+  },
+  {
+    id: 45,
+    name: "Amy Farrah Fowler",
+    series: "The Big Bang Theory",
+    image:
+      "https://static.wikia.nocookie.net/bigbangtheory/images/c/cc/Amy_Farrah_Fowler.jpg",
+    appearances: ["The Big Bang Theory"],
+  },
+  {
+    id: 46,
+    name: "Bernadette Rostenkowski",
+    series: "The Big Bang Theory",
+    image:
+      "https://static.wikia.nocookie.net/bigbangtheory/images/1/1d/Bernadette_Rostenkowski.jpg",
+    appearances: ["The Big Bang Theory"],
+  },
+  {
+    id: 47,
+    name: "Pam Beesly",
+    series: "The Office",
+    image:
+      "https://static.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesly.jpg",
+    appearances: ["The Office"],
+  },
+  {
+    id: 48,
+    name: "Angela Martin",
+    series: "The Office",
+    image:
+      "https://static.wikia.nocookie.net/theoffice/images/0/0b/Angela_Martin.jpg",
+    appearances: ["The Office"],
+  },
+  {
+    id: 49,
+    name: "Summer Smith",
+    series: "Rick and Morty",
+    image:
+      "https://static.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_Smith.jpg",
+    appearances: ["Rick and Morty"],
+  },
+  {
+    id: 50,
+    name: "Beth Smith",
+    series: "Rick and Morty",
+    image:
+      "https://static.wikia.nocookie.net/rickandmorty/images/5/58/Beth_Smith.jpg",
+    appearances: ["Rick and Morty"],
+  },
+  {
+    id: 51,
+    name: "Jerry Smith",
+    series: "Rick and Morty",
+    image:
+      "https://static.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.jpg",
+    appearances: ["Rick and Morty"],
+  },
+  {
+    id: 52,
+    name: "Mike Ehrmantraut",
+    series: "Breaking Bad",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/8/8b/Mike_Ehrmantraut.jpg",
+    appearances: ["Breaking Bad", "Better Call Saul"],
+  },
+  {
+    id: 53,
+    name: "Kim Wexler",
+    series: "Better Call Saul",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/f/f7/Kim_Wexler.jpg",
+    appearances: ["Better Call Saul"],
+  },
+  {
+    id: 54,
+    name: "Sansa Stark",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/7/7e/Sansa_Stark.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 55,
+    name: "Jaime Lannister",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/e/eb/Jaime_Lannister.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 56,
+    name: "Negan",
+    series: "The Walking Dead",
+    image:
+      "https://static.wikia.nocookie.net/walkingdead/images/5/58/Negan.jpg",
+    appearances: ["The Walking Dead"],
+  },
+  {
+    id: 57,
+    name: "Carol Peletier",
+    series: "The Walking Dead",
+    image:
+      "https://static.wikia.nocookie.net/walkingdead/images/a/a2/Carol_Peletier.jpg",
+    appearances: ["The Walking Dead"],
+  },
+  {
+    id: 58,
+    name: "Max Mayfield",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/4/41/Max_Mayfield.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 59,
+    name: "Lucas Sinclair",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/8/82/Lucas_Sinclair.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 60,
+    name: "Will Byers",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/d/d5/Will_Byers.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 61,
+    name: "Nancy Wheeler",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/e/ea/Nancy_Wheeler.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 62,
+    name: "Joyce Byers",
+    series: "Stranger Things",
+    image:
+      "https://static.wikia.nocookie.net/strangerthings/images/8/8a/Joyce_Byers.jpg",
+    appearances: ["Stranger Things"],
+  },
+  {
+    id: 63,
+    name: "Debra Morgan",
+    series: "Dexter",
+    image:
+      "https://static.wikia.nocookie.net/dexter/images/b/b5/Debra_Morgan.jpg",
+    appearances: ["Dexter"],
+  },
+  {
+    id: 64,
+    name: "Rita Bennett",
+    series: "Dexter",
+    image:
+      "https://static.wikia.nocookie.net/dexter/images/3/3d/Rita_Bennett.jpg",
+    appearances: ["Dexter"],
+  },
+  {
+    id: 65,
+    name: "James Gordon",
+    series: "Gotham",
+    image:
+      "https://static.wikia.nocookie.net/gotham-series/images/8/8d/James_Gordon.jpg",
+    appearances: ["Gotham"],
+  },
+  {
+    id: 66,
+    name: "Bruce Wayne",
+    series: "Gotham",
+    image:
+      "https://static.wikia.nocookie.net/gotham-series/images/b/bf/Bruce_Wayne.jpg",
+    appearances: ["Gotham"],
+  },
+  {
+    id: 67,
+    name: "Oswald Cobblepot",
+    series: "Gotham",
+    image:
+      "https://static.wikia.nocookie.net/gotham-series/images/c/c3/Oswald_Cobblepot.jpg",
+    appearances: ["Gotham"],
+  },
+  {
+    id: 68,
+    name: "Tommy Shelby",
+    series: "Peaky Blinders",
+    image:
+      "https://static.wikia.nocookie.net/peaky-blinders/images/7/7a/Tommy_Shelby.jpg",
+    appearances: ["Peaky Blinders"],
+  },
+  {
+    id: 69,
+    name: "Arthur Shelby",
+    series: "Peaky Blinders",
+    image:
+      "https://static.wikia.nocookie.net/peaky-blinders/images/5/52/Arthur_Shelby.jpg",
+    appearances: ["Peaky Blinders"],
+  },
+  {
+    id: 70,
+    name: "Polly Gray",
+    series: "Peaky Blinders",
+    image:
+      "https://static.wikia.nocookie.net/peaky-blinders/images/8/8e/Polly_Gray.jpg",
+    appearances: ["Peaky Blinders"],
+  },
+  {
+    id: 71,
+    name: "Ragnar Lothbrok",
+    series: "Vikings",
+    image:
+      "https://static.wikia.nocookie.net/vikings/images/1/1f/Ragnar_Lothbrok.jpg",
+    appearances: ["Vikings"],
+  },
+  {
+    id: 72,
+    name: "Lagertha",
+    series: "Vikings",
+    image: "https://static.wikia.nocookie.net/vikings/images/7/7a/Lagertha.jpg",
+    appearances: ["Vikings"],
+  },
+  {
+    id: 73,
+    name: "Bjorn Ironside",
+    series: "Vikings",
+    image:
+      "https://static.wikia.nocookie.net/vikings/images/5/5f/Bjorn_Ironside.jpg",
+    appearances: ["Vikings"],
+  },
+  {
+    id: 74,
+    name: "Elliot Alderson",
+    series: "Mr. Robot",
+    image:
+      "https://static.wikia.nocookie.net/mrrobot/images/8/8b/Elliot_Alderson.jpg",
+    appearances: ["Mr. Robot"],
+  },
+  {
+    id: 75,
+    name: "Darlene Alderson",
+    series: "Mr. Robot",
+    image:
+      "https://static.wikia.nocookie.net/mrrobot/images/d/d4/Darlene_Alderson.jpg",
+    appearances: ["Mr. Robot"],
+  },
+  {
+    id: 76,
+    name: "Mr. Robot",
+    series: "Mr. Robot",
+    image: "https://static.wikia.nocookie.net/mrrobot/images/3/39/Mr_Robot.jpg",
+    appearances: ["Mr. Robot"],
+  },
+  {
+    id: 77,
+    name: "Oliver Queen",
+    series: "Arrow",
+    image:
+      "https://static.wikia.nocookie.net/arrow/images/7/74/Oliver_Queen.jpg",
+    appearances: ["Arrow"],
+  },
+  {
+    id: 78,
+    name: "Felicity Smoak",
+    series: "Arrow",
+    image:
+      "https://static.wikia.nocookie.net/arrow/images/8/85/Felicity_Smoak.jpg",
+    appearances: ["Arrow"],
+  },
+  {
+    id: 79,
+    name: "John Diggle",
+    series: "Arrow",
+    image:
+      "https://static.wikia.nocookie.net/arrow/images/6/6d/John_Diggle.jpg",
+    appearances: ["Arrow"],
+  },
+  {
+    id: 80,
+    name: "Barry Allen",
+    series: "The Flash",
+    image:
+      "https://static.wikia.nocookie.net/theflash/images/2/2b/Barry_Allen.jpg",
+    appearances: ["The Flash"],
+  },
+  {
+    id: 81,
+    name: "Iris West-Allen",
+    series: "The Flash",
+    image:
+      "https://static.wikia.nocookie.net/theflash/images/5/5a/Iris_West-Allen.jpg",
+    appearances: ["The Flash"],
+  },
+  {
+    id: 82,
+    name: "Cisco Ramon",
+    series: "The Flash",
+    image:
+      "https://static.wikia.nocookie.net/theflash/images/3/3b/Cisco_Ramon.jpg",
+    appearances: ["The Flash"],
+  },
+  {
+    id: 83,
+    name: "Elizabeth Keen",
+    series: "The Blacklist",
+    image:
+      "https://static.wikia.nocookie.net/blacklist/images/2/24/Elizabeth_Keen.jpg",
+    appearances: ["The Blacklist"],
+  },
+  {
+    id: 84,
+    name: "Dembe Zuma",
+    series: "The Blacklist",
+    image:
+      "https://static.wikia.nocookie.net/blacklist/images/9/96/Dembe_Zuma.jpg",
+    appearances: ["The Blacklist"],
+  },
+  {
+    id: 85,
+    name: "Harold Cooper",
+    series: "The Blacklist",
+    image:
+      "https://static.wikia.nocookie.net/blacklist/images/1/15/Harold_Cooper.jpg",
+    appearances: ["The Blacklist"],
+  },
+  {
+    id: 86,
+    name: "Creed Bratton",
+    series: "The Office",
+    image:
+      "https://static.wikia.nocookie.net/theoffice/images/2/20/Creed_Bratton.jpg",
+    appearances: ["The Office"],
+  },
+  {
+    id: 87,
+    name: "Stanley Hudson",
+    series: "The Office",
+    image:
+      "https://static.wikia.nocookie.net/theoffice/images/2/23/Stanley_Hudson.jpg",
+    appearances: ["The Office"],
+  },
+  {
+    id: 88,
+    name: "Kevin Malone",
+    series: "The Office",
+    image:
+      "https://static.wikia.nocookie.net/theoffice/images/b/b2/Kevin_Malone.jpg",
+    appearances: ["The Office"],
+  },
+  {
+    id: 89,
+    name: "Todd Alquist",
+    series: "Breaking Bad",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/9/95/Todd_Alquist.jpg",
+    appearances: ["Breaking Bad", "El Camino"],
+  },
+  {
+    id: 90,
+    name: "Skyler White",
+    series: "Breaking Bad",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/b/be/Skyler_White.jpg",
+    appearances: ["Breaking Bad"],
+  },
+  {
+    id: 91,
+    name: "Hank Schrader",
+    series: "Breaking Bad",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/8/8b/Hank_Schrader.jpg",
+    appearances: ["Breaking Bad"],
+  },
+  {
+    id: 92,
+    name: "Chuck McGill",
+    series: "Better Call Saul",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/3/3e/Chuck_McGill.jpg",
+    appearances: ["Better Call Saul"],
+  },
+  {
+    id: 93,
+    name: "Howard Hamlin",
+    series: "Better Call Saul",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/5/57/Howard_Hamlin.jpg",
+    appearances: ["Better Call Saul"],
+  },
+  {
+    id: 94,
+    name: "Nacho Varga",
+    series: "Better Call Saul",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/9/9f/Nacho_Varga.jpg",
+    appearances: ["Better Call Saul"],
+  },
+  {
+    id: 95,
+    name: "Bran Stark",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/d/d8/Bran_Stark.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 96,
+    name: "Joffrey Baratheon",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/2/25/Joffrey_Baratheon.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 97,
+    name: "Samwell Tarly",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/5/5b/Samwell_Tarly.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 98,
+    name: "Brienne of Tarth",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/4/4b/Brienne_of_Tarth.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 99,
+    name: "Sandor Clegane",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/7/74/Sandor_Clegane.jpg",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 100,
+    name: "Petyr Baelish",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/5/5e/Petyr_Baelish.jpg",
+    appearances: ["Game of Thrones"],
+  },
 ];
