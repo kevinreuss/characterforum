1e749d9612737aeff21f2b34599ad94926deeb33
Kevin Reuss <kevreuss@gmail.com>
2025-03-29 01:06:58 +0100

    imprive

---
+++ characters.js
@@ -0,0 +1,42 @@
+const characters = [
+  {
+    id: 1,
+    name: "Charlie Harper",
+    series: "Two and a Half Men",
+    image:
+      "https://static.wikia.nocookie.net/twoandahalfmen/images/e/e7/Charlie_Harper.jpg",
+    appearances: ["Two and a Half Men"],
+  },
+  {
+    id: 2,
+    name: "Walter White",
+    series: "Breaking Bad",
+    image:
+      "https://static.wikia.nocookie.net/breakingbad/images/5/54/Walter_White_S5B.png",
+    appearances: ["Breaking Bad"],
+  },
+  {
+    id: 3,
+    name: "Tyrion Lannister",
+    series: "Game of Thrones",
+    image:
+      "https://static.wikia.nocookie.net/gameofthrones/images/9/95/HandoftheKingTyrionLannister.PNG",
+    appearances: ["Game of Thrones"],
+  },
+  {
+    id: 4,
+    name: "Sheldon Cooper",
+    series: "The Big Bang Theory",
+    image:
+      "https://static.wikia.nocookie.net/bigbangtheory/images/6/6a/Sheldon_Cooper.jpg",
+    appearances: ["The Big Bang Theory"],
+  },
+  {
+    id: 5,
+    name: "Michael Scott",
+    series: "The Office",
+    image:
+      "https://static.wikia.nocookie.net/theoffice/images/0/02/Michael_Scott.jpg",
+    appearances: ["The Office"],
+  },
+];
